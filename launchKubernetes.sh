d2c804fd8633e4fbca7bade9fe5ddd9f2a8248a8
Ubuntu <tom@KubernetesMachine.sbhpxfwmtfse5owuc0sdgr244b.zx.internal.cloudapp.net>
2021-01-07 16:09:13 +0000

    eod commit

--- launchKubernetes.sh
+++ launchKubernetes.sh
@@ -1,4 +1,4 @@
 #https://kubernetes.io/blog/2019/07/23/get-started-with-kubernetes-using-python/
 #kubectl apply -f deployment.yaml
 kubectl apply -f mysql-pv.yaml
-sed 's/{{password}}/$PASSWORD/g;s/{{secret-key}}/$SECRET_KEY/g' deployment.yaml | kubectl apply -f -
+sed 's/{{password}}/$PASSWORD/g;s/{{secret-key}}/$SECRET_KEY/g;s/{{db_uri}}/$PASSWORD/g' deployment.yaml | kubectl apply -f -
