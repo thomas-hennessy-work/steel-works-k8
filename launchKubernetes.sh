262c942ce8bf0b0f254c6d15aa1ce588abdf594f
Ubuntu <tom@KubernetesMachine.sbhpxfwmtfse5owuc0sdgr244b.zx.internal.cloudapp.net>
2021-01-07 14:36:59 +0000

    database created on startup (not connected)

--- launchKubernetes.sh
+++ launchKubernetes.sh
@@ -1,2 +1,4 @@
 #https://kubernetes.io/blog/2019/07/23/get-started-with-kubernetes-using-python/
-kubectl run 
+#kubectl apply -f deployment.yaml
+kubectl apply -f mysql-pv.yaml
+sed 's/{{password}}/root/g' deployment.yaml | kubectl apply -f -
